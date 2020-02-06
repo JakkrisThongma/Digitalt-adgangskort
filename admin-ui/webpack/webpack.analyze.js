c97f44a40c50846ded45f8c5b659c93ca659e76c
Mohamad Kenas <m.kenas91@gmail.com>
2020-02-06 10:33:18 +0100

    Setup webpack bundler

---
+++ admin-ui/webpack/webpack.analyze.js
@@ -0,0 +1,10 @@
+const merge = require('webpack-merge');
+const WebpackBundleAnalyzer = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
+
+const prodConfig = require('./webpack.prod.js');
+
+module.exports = merge(prodConfig, {
+  mode: 'none',
+  plugins: [new WebpackBundleAnalyzer()]
+});
