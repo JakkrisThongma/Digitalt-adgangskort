731dcbfa954c61d781cd069ab4c78008482b02ac
Mohamad Kenas <s320045@oslomet.no>
2020-02-13 15:51:25 +0000

    Updated locals.tf

--- admin-ui/Infrastructure/locals.tf
+++ admin-ui/Infrastructure/locals.tf
@@ -1,5 +1,5 @@
 locals {    
-    resource_group_name = "${var.resource_group_name}-${var.environment}-rg"
+    resource_group_name = "stud-gruppe5-oppgave5-dwe-rg"
     app_service_plan_name = "${var.web_app_name}"
     web_app_name = "${var.web_app_name}"
     autoscale_settings_name = "${var.web_app_name}"
