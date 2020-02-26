1215edb83d843d4795fcd8142d60fdea8b4c5628
Mohamad Kenas <m.kenas91@gmail.com>
2020-02-26 18:21:18 +0100

    Change name for home component to dashboard

--- admin-ui/src/views/index.js
+++ admin-ui/src/views/index.js
@@ -1,4 +1,4 @@
-export { default as Home } from "./Home";
+export { default as Dashboard } from "./Dashboard";
 export { default as UserList } from "./UserList";
 export { default as LockList } from "./LockList";
 export { default as AccessLevelList } from "./AccessLevelList";
