0f8a99ec5111ae04eab0c2fad87d407743103d8e
Mohamad Kenas <m.kenas91@gmail.com>
2020-02-29 00:34:23 +0100

    Move Dashborad view to its own folder

--- admin-ui/src/views/index.js
+++ admin-ui/src/views/index.js
@@ -1,4 +1,4 @@
-export { default as Dashboard } from "./Dashboard";
+export { default as Dashboard } from "./Dashboard/Dashboard";
 export { default as UserList } from "./UserList";
 export { default as LockList } from "./LockList";
 export { default as AccessLevelList } from "./AccessLevelList";
