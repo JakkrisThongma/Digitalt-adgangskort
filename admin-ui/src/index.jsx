2f724ec6a267932471e5df6919feddb93f051433
Mohamad Kenas <m.kenas91@gmail.com>
2020-03-18 18:05:45 +0100

    Apply adal authentication

--- admin-ui/src/index.jsx
+++ admin-ui/src/index.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { runWithAdal } from "react-adal";
 import App from "./App";
+
+import { authContext } from "./auth/adalConfig";
+
+const DO_NOT_LOGIN = false;
+runWithAdal(
+  authContext,
+  () => {
+    require("./App");
+  },
+  DO_NOT_LOGIN
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
