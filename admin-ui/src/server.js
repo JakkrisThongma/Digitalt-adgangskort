5d7ccb8dcffd861aaee90fd849b9ec376428dff2
Mohamad Kenas <m.kenas91@gmail.com>
2020-03-18 18:43:26 +0100

    Add https server

--- admin-ui/src/server.js
+++ admin-ui/src/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const path = require("path");
 
+const fs = require("fs");
+const https = require("https");
+
 const app = express();
 
 if (process.env.NODE_ENV !== "production") {
@@ -10,11 +13,27 @@ if (process.env.NODE_ENV !== "production") {
   process.exit(1);
 }
 
+https
+  .createServer(
+    {
+      key: fs.readFileSync("server.key"),
+      cert: fs.readFileSync("server.cert")
+    },
+    app
+  )
+  .listen(44321, function() {
+    console.log(
+      "Admin UI is listening on port 44321! Go to https://localhost:44321/"
+    );
+  });
+
 app.use(express.static("dist"));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
+/*
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`http://localhost:${port}`));
+*/
