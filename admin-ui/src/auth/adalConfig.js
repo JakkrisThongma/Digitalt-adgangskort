08d041ff952b0802b0f3d4a594c2a423bd62d8c5
Mohamad Kenas <m.kenas91@gmail.com>
2020-03-21 20:58:48 +0100

    Add helper functions

--- admin-ui/src/auth/adalConfig.js
+++ admin-ui/src/auth/adalConfig.js
@@ -1,9 +1,9 @@
-import { AuthenticationContext } from "react-adal";
+import { AuthenticationContext, adalFetch, withAdalLogin } from "react-adal";
 
 export const adalConfig = {
   tenant: "3602e498-fd34-4a1e-a61b-b4639ceeaffd",
   clientId: "93a5c726-983f-4e8c-a330-f1b34e3d6752",
-  redirectUri: "https://localhost:44321/signin-oidc",
+  redirectUri: "https://kenas-web-app.azurewebsites.net/signin-oidc",
   endpoints: {
     api: "3602e498-fd34-4a1e-a61b-b4639ceeaffd"
   },
@@ -12,3 +12,10 @@ export const adalConfig = {
 
 export const authContext = new AuthenticationContext(adalConfig);
 export const getToken = () => authContext.getCachedToken(adalConfig.clientId);
+export const adalApiFetch = (fetch, url, options) =>
+  adalFetch(authContext, adalConfig.endpoints.api, fetch, url, options);
+
+export const withAdalLoginApi = withAdalLogin(
+  authContext,
+  adalConfig.endpoints.api
+);
