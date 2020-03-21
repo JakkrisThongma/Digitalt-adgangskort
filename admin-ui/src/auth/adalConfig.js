44f1711a1461514d003899b4464397f1ecc8c67a
Mohamad Kenas <m.kenas91@gmail.com>
2020-03-21 21:06:54 +0100

    Change redirect uri

--- admin-ui/src/auth/adalConfig.js
+++ admin-ui/src/auth/adalConfig.js
@@ -3,7 +3,8 @@ import { AuthenticationContext, adalFetch, withAdalLogin } from "react-adal";
 export const adalConfig = {
   tenant: "3602e498-fd34-4a1e-a61b-b4639ceeaffd",
   clientId: "93a5c726-983f-4e8c-a330-f1b34e3d6752",
-  redirectUri: "https://kenas-web-app.azurewebsites.net/signin-oidc",
+  redirectUri:
+    "https://digitaladgangskortadminui-dev-web.azurewebsites.net/.auth/login/aad/callback",
   endpoints: {
     api: "3602e498-fd34-4a1e-a61b-b4639ceeaffd"
   },
