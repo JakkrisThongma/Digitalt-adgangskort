deba37e629620cedb4426c99a219e486c52ca5a1
Mohamad Kenas <m.kenas91@gmail.com>
2020-03-21 20:58:16 +0100

    Import web config

--- admin-ui/src/index.jsx
+++ admin-ui/src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { runWithAdal } from "react-adal";
 import App from "./App";
-
+import "./web.config";
 import { authContext } from "./auth/adalConfig";
 
 const DO_NOT_LOGIN = false;
