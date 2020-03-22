ce78af4f434abae97c8c42b5963e85a052934161
Mohamad Kenas <m.kenas91@gmail.com>
2020-03-22 13:20:46 +0100

    Fix hardcoded values

--- admin-ui/Infrastructure/main.tf
+++ admin-ui/Infrastructure/main.tf
@@ -1,24 +1,13 @@
 data "azurerm_client_config" "current" {}
 
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "stud-shared-dwe-rg"
-    storage_account_name = "studshareddwesa"
-    container_name       = "gruppe5"
-    key                  = "gruppe5.tfstate"
-  }
+  backend "azurerm" {}
 }
-/*
-resource "azurerm_resource_group" "gruppe-rsg" {
-  name     = "${local.resource_group_name}"
-  location = "${var.location}"
-}
-*/
+
 
 module "web_app" {
   source  = "innovationnorway/web-app/azurerm"
   version = "0.1.6"
-
 
   resource_group_name = "${var.resource_group_name}"
   location            = "${var.location}"
@@ -32,5 +21,3 @@ module "web_app" {
 
   tags = "${var.tags}"
 }
-
-
