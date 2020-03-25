252f58a7525e746948458c858361d0f1872003a6
Mohamad Kenas <m.kenas91@gmail.com>
2020-03-25 15:09:25 +0100

    Update pipeline

--- admin-ui/Infrastructure/locals.tf
+++ admin-ui/Infrastructure/locals.tf
@@ -1,6 +1,11 @@
 
 locals {    
     resource_group_name = "{var.resource_group_name}-${var.environment}-rg"
+
+    app_service_plan_name = "${var.ui_web_app2_name}"
+    web_app_name = "${var.ui_web_app2_name}"
+    autoscale_settings_name = "${var.ui_web_app2_name}"
+
     ui_app_service_plan_name = "${var.ui_web_app_name}"
     ui_web_app_name = "${var.ui_web_app_name}"
     ui_autoscale_settings_name = "${var.ui_web_app_name}"
--- admin-ui/Infrastructure/main.tf
+++ admin-ui/Infrastructure/main.tf
@@ -40,3 +40,19 @@ module "api_web_app" {
 }
 
 
+module "web_app2" {
+  source  = "innovationnorway/web-app/azurerm"
+  version = "0.1.6"
+
+  resource_group_name = "${var.resource_group_name}"
+  location            = "${var.location}"
+  web_app_name        = "${var.web_app2_name}"
+  environment         = "${var.environment}"
+  release             = "${var.release}"
+
+  sku_size                    = "${var.sku_size}"
+  sku_tier                    = "${var.sku_tier}"
+  default_autoscale_instances = "${var.default_autoscale_instances}"
+
+  tags = "${var.tags}"
+}
