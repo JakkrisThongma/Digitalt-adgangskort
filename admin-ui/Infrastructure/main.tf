809e89175c2541a2bec9d75310b75ee39a60fc57
Mohamad Kenas <m.kenas91@gmail.com>
2020-03-25 11:12:52 +0100

    Add  API app service to the pipeline

--- admin-ui/Infrastructure/main.tf
+++ admin-ui/Infrastructure/main.tf
@@ -21,3 +21,20 @@ module "web_app" {
 
   tags = "${var.tags}"
 }
+
+module "web_app" {
+  source  = "innovationnorway/web-app/azurerm"
+  version = "0.1.6"
+
+  resource_group_name = "${var.resource_group_name}"
+  location            = "${var.location}"
+  web_app_name        = "${var.web_app2_name}"
+  environment         = "${var.environment}"
+  release             = "${var.release}"
+
+  sku_size                    = "${var.sku_size}"
+  sku_tier                    = "${var.sku_tier}"
+  default_autoscale_instances = "${var.default_autoscale_instances}"
+
+  tags = "${var.tags}"
+}
--- admin-ui/Infrastructure/myVars.tfvars
+++ admin-ui/Infrastructure/myVars.tfvars
@@ -1,4 +1,5 @@
 web_app_name = "digitalAdgangskortAdminUI"
+web_app2_name="digitalAdgangskortAdminAPI"
 resource_group_name = "stud-gruppe5-oppgave5-dwe-rg"
 environment = "dev"
 
