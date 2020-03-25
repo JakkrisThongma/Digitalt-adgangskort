09a28f989061e4d6b1e1d1fefbeb8b2517a19f9e
Mohamad Kenas <m.kenas91@gmail.com>
2020-03-25 20:27:25 +0100

    Update pipiline

--- admin-ui/Infrastructure/locals.tf
+++ admin-ui/Infrastructure/locals.tf
@@ -1,9 +1,6 @@
 
 locals {    
     resource_group_name = "{var.resource_group_name}-${var.environment}-rg"
-    app_service_plan_name = "${var.web_app_name}"
-    web_app_name = "${var.web_app_name}"
-    autoscale_settings_name = "${var.web_app_name}"
 
     ui_app_service_plan_name = "${var.ui_web_app_name}"
     ui_web_app_name = "${var.ui_web_app_name}"
