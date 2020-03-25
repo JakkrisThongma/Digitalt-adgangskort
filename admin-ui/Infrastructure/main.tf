3ba6386681a36d483f1f08c0c077c584dd32efa8
Mohamad Kenas <m.kenas91@gmail.com>
2020-03-25 15:16:22 +0100

    Update pipeline

--- admin-ui/Infrastructure/main.tf
+++ admin-ui/Infrastructure/main.tf
@@ -38,21 +38,3 @@ module "api_web_app" {
 
   tags = "${var.tags}"
 }
-
-
-module "web_app2" {
-  source  = "innovationnorway/web-app/azurerm"
-  version = "0.1.6"
-
-  resource_group_name = "${var.resource_group_name}"
-  location            = "${var.location}"
-  web_app_name        = "${var.web_app2_name}"
-  environment         = "${var.environment}"
-  release             = "${var.release}"
-
-  sku_size                    = "${var.sku_size}"
-  sku_tier                    = "${var.sku_tier}"
-  default_autoscale_instances = "${var.default_autoscale_instances}"
-
-  tags = "${var.tags}"
-}
--- admin-ui/Infrastructure/myVars.tfvars
+++ admin-ui/Infrastructure/myVars.tfvars
@@ -1,4 +1,3 @@
-web_app2_name="digitalAdgangskortWebAPI"
 ui_web_app_name = "digital-adgangs-kort-admin-ui"
 api_web_app_name="digital-adgangs-kort-web-api"
 resource_group_name = "stud-gruppe5-oppgave5-dwe-rg"
