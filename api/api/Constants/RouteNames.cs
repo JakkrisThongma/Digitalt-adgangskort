485f33b0108f974868e7adde3d5ef8e8361d6d77
Mohamad Kenas <m.kenas91@gmail.com>
2020-04-07 15:46:19 +0200

    Add Route Names class

---
+++ api/api/Constants/RouteNames.cs
@@ -0,0 +1,13 @@
+namespace api.Constants
+{
+    public class RouteNames
+    {
+        public const string Users = nameof(Users);
+        public const string UserById = nameof(UserById);
+        public const string PhotoByUserId = nameof(PhotoByUserId);
+
+        public const string Groups = nameof(Groups);
+        public const string GroupById = nameof(GroupById);
+        public const string GroupMembersByGroupId = nameof(GroupMembersByGroupId);
+    }
+}
