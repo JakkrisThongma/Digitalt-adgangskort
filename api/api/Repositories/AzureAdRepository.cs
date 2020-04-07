d5d673618fca3e0de366f6d50049be79e104138f
Mohamad Kenas <m.kenas91@gmail.com>
2020-04-07 15:43:14 +0200

    Add Azure Ad Repository

---
+++ api/api/Repositories/AzureAdRepository.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+
+namespace api.Repositories
+{
+    public class AzureAdRepository : IAzureAdRepository
+    {
+        public async Task<User> GetUser(GraphServiceClient client, string userId)
+        {
+            if (client == null)
+            {
+                throw new ArgumentNullException(nameof(client));
+            }
+            if (userId == null)
+            {
+                throw new ArgumentNullException(nameof(userId));
+            }
+
+            var user = await client.Users[userId].Request().GetAsync();
+            return user;
+        }
+
+        public async Task<Stream> GetUserPhoto(GraphServiceClient client, string userId)
+        {
+            if (client == null)
+            {
+                throw new ArgumentNullException(nameof(client));
+            }
+            if (userId == null)
+            {
+                throw new ArgumentNullException(nameof(userId));
+            }
+
+            var pictureStream = await client.Users[userId].Photo.Content.Request().GetAsync();
+
+            return pictureStream;
+        }
+
+
+        public async Task<IGraphServiceUsersCollectionPage> GetUsers(GraphServiceClient client)
+        {
+            if (client == null)
+            {
+                throw new ArgumentNullException(nameof(client));
+            }
+            var userList = await client.Users.Request().GetAsync();
+
+            return userList;
+        }
+        
+
+        public async Task<Group> GetGroup(GraphServiceClient client, string groupId)
+        {
+            if (client == null)
+            {
+                throw new ArgumentNullException(nameof(client));
+            }
+            if (groupId == null)
+            {
+                throw new ArgumentNullException(nameof(groupId));
+            }
+            var group = await client.Groups[groupId].Request().GetAsync();
+
+            return group;
+        }
+
+
+        public async Task<IGroupMembersCollectionWithReferencesPage> GetGroupMembers(GraphServiceClient client,
+            string groupId)
+        {
+            if (client == null)
+            {
+                throw new ArgumentNullException(nameof(client));
+            }
+            if (groupId == null)
+            {
+                throw new ArgumentNullException(nameof(groupId));
+            }
+            var members = await client.Groups[groupId]
+                .Members
+                .Request()
+                .GetAsync();
+            
+            return members;
+        }
+
+
+        public async Task<IGraphServiceGroupsCollectionPage> GetGroups(GraphServiceClient client)
+        {
+            if (client == null)
+            {
+                throw new ArgumentNullException(nameof(client));
+            }
+            var groupList = await client.Groups.Request().GetAsync();
+            return groupList;
+        }
+    }
+}
