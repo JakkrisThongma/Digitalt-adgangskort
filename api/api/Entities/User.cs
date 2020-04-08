91ac54618f8c17768be726bbe94ef5633a3372da
Mohamad Kenas <m.kenas91@gmail.com>
2020-04-08 16:36:06 +0200

    Update User entity

--- api/api/Entities/User.cs
+++ api/api/Entities/User.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using api.Types;
 
 namespace api.Entities
 {
@@ -11,9 +12,17 @@ namespace api.Entities
         public User()
         {
             SmartLockUsers = new List<SmartLockUser>();
+            CreationDate = new DateTimeOffset(DateTime.Now);
         }
+        
+        [Key] 
+        public Guid Id { get; set; }
 
-        [Key] public Guid Id { get; set; }
+      
+        public Status Status { get; set; }
+        public DateTimeOffset CreationDate { get; set; }
+        public DateTimeOffset LastModificationDate { get; set; }
+        
 
         public ICollection<SmartLockUser> SmartLockUsers { get; set; }
     }
