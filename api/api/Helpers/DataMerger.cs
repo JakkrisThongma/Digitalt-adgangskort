a95ee137482bfae9749f1eaf0c6cd1a3e40a7b1b
Mohamad Kenas <m.kenas91@gmail.com>
2020-04-11 23:55:55 +0200

    Update data merger

--- api/api/Helpers/DataMerger.cs
+++ api/api/Helpers/DataMerger.cs
@@ -9,34 +9,35 @@ namespace api.Helpers
 {
     public class DataMerger
     {
-
-        public static UserDto MergeUserWithAzureData(User userFromRepo, Microsoft.Graph.User userFromAzureAd, IMapper  mapper)
+        public static UserDto MergeUserWithAzureData(User userFromRepo, Microsoft.Graph.User userFromAzureAd,
+            IMapper mapper)
         {
             var dtoFromDb = mapper.Map<UserDto>(userFromRepo);
             return mapper.Map(userFromAzureAd, dtoFromDb);
         }
-        
-        public static IEnumerable<UserDto> MergeUsersWithAzureData(IEnumerable<User> usersFromRepo, 
-            Microsoft.Graph.IGraphServiceUsersCollectionPage allUsersFromAzureAd, IMapper  mapper)
+
+        public static IEnumerable<UserDto> MergeUsersWithAzureData(IEnumerable<User> usersFromRepo,
+            Microsoft.Graph.IGraphServiceUsersCollectionPage allUsersFromAzureAd, IMapper mapper)
         {
             var mergedUsers = (from userFromRepo in usersFromRepo
-                from dbUserFromAzureAd in allUsersFromAzureAd 
-                where userFromRepo.Id == Guid.Parse(dbUserFromAzureAd.Id) 
-                let dtoFromDb = mapper.Map<UserDto>(userFromRepo) 
+                from dbUserFromAzureAd in allUsersFromAzureAd
+                where userFromRepo.Id == Guid.Parse(dbUserFromAzureAd.Id)
+                let dtoFromDb = mapper.Map<UserDto>(userFromRepo)
                 select mapper.Map(dbUserFromAzureAd, dtoFromDb));
 
             return mergedUsers;
         }
-        
-        public static GroupDto MergeGroupWithAzureData(Group groupFromRepo, Microsoft.Graph.Group groupFromAzureAd, IMapper  mapper)
+
+        public static GroupDto MergeGroupWithAzureData(Group groupFromRepo, Microsoft.Graph.Group groupFromAzureAd,
+            IMapper mapper)
         {
             var dtoGroupFromDb = mapper.Map<GroupDto>(groupFromRepo);
 
             return mapper.Map(groupFromAzureAd, dtoGroupFromDb);
         }
-        
-        public static IEnumerable<GroupDto> MergeGroupsWithAzureData(IEnumerable<Group> groupsFromRepo, 
-            Microsoft.Graph.IGraphServiceGroupsCollectionPage allGroupsFromAzureAd, IMapper  mapper)
+
+        public static IEnumerable<GroupDto> MergeGroupsWithAzureData(IEnumerable<Group> groupsFromRepo,
+            Microsoft.Graph.IGraphServiceGroupsCollectionPage allGroupsFromAzureAd, IMapper mapper)
         {
             var mergedGroups = (from groupFromRepo in groupsFromRepo
                 from dbGroupFromAzureAd in allGroupsFromAzureAd
@@ -46,9 +47,9 @@ namespace api.Helpers
 
             return mergedGroups;
         }
-        
-        public static IEnumerable<GroupDto> MergeGroupsWithAzureData(IEnumerable<Group> groupsFromRepo, 
-            IEnumerable<Microsoft.Graph.Group> allGroupsFromAzureAd, IMapper  mapper)
+
+        public static IEnumerable<GroupDto> MergeGroupsWithAzureData(IEnumerable<Group> groupsFromRepo,
+            IEnumerable<Microsoft.Graph.Group> allGroupsFromAzureAd, IMapper mapper)
         {
             var mergedGroups = (from groupFromRepo in groupsFromRepo
                 from dbGroupFromAzureAd in allGroupsFromAzureAd
@@ -57,6 +58,29 @@ namespace api.Helpers
                 select mapper.Map(dbGroupFromAzureAd, dtoFromDb));
 
             return mergedGroups;
+        }
+
+        public static List<string> MergeLists(List<string> list1, List<string> list2)
+        {
+            if (list1.Count > 0 && list2.Count > 0 )
+            {
+                foreach (var item in list2)
+                {
+                    if (!list1.Contains(item))
+                    {
+                        list1.Add(item);
+                    }
+                }
+               
+                return list1;
+            }
+            if (list1.Count > 0 )
+            {
+                return list1;
+
+            }
+            
+            return list2;
         }
     }
 }
