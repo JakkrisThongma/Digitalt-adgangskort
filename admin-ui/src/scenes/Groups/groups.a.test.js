69380706f6a2fe366558e64f8e1dbd42ffa1bb33
Mohamad Kenas <m.kenas91@gmail.com>
2020-05-04 16:10:26 +0200

    Fix bug was preventing from running test in parallel

--- admin-ui/src/scenes/Groups/groups.a.test.js
+++ admin-ui/src/scenes/Groups/groups.a.test.js
@@ -5,7 +5,6 @@ import {
   getGroups,
   deleteGroup,
   updateGroup,
-  getSmartLocks,
   addSmartLock,
   deleteSmartLock,
   getGroupSmartLocks
@@ -14,40 +13,172 @@ import helpers from "../../helpers";
 
 const { extractIdList } = helpers;
 
-describe("Groups", function() {
-  let firstGroupFromAzureAd;
-  let secondGroupFromAzureAd;
-  let groupCount;
-  let smartLocks;
-  let updatedSmartLocks;
-  const smartLocksToAdd = [
-    {
-      title: "Smart lock 1",
-      status: "Active"
-    },
-    {
-      title: "Smart lock 2",
-      status: "Inactive"
-    }
-  ];
+let firstGroupFromAzureAd;
+let secondGroupFromAzureAd;
+let groupCount;
+let smartLocks;
+let firstSmartLock;
+let secondeSmartLock;
+let updatedSmartLocks;
+const smartLocksToAdd = [
+  {
+    title: "Smart lock 1",
+    status: "Active"
+  },
+  {
+    title: "Smart lock 2",
+    status: "Inactive"
+  }
+];
 
-  beforeAll(async done => {
+describe("Setup", function() {
+  it("should set tests data", async function(done) {
     const azureAdGroups = await getAzureAdGroups();
     [firstGroupFromAzureAd, secondGroupFromAzureAd] = await azureAdGroups.data;
 
     const groups = await getGroups();
     groupCount = await groups.data.length;
-    await addSmartLock(smartLocksToAdd[0]);
-    await addSmartLock(smartLocksToAdd[1]);
-    const smartLocksResponse = await getSmartLocks();
-    smartLocks = smartLocksResponse.data;
+    const addFirstSmartLock = await addSmartLock(smartLocksToAdd[0]);
+    firstSmartLock = addFirstSmartLock.data;
+    const addSecondSmartLock = await addSmartLock(smartLocksToAdd[1]);
+    secondeSmartLock = addSecondSmartLock.data;
+    smartLocks = [firstSmartLock, secondeSmartLock];
     updatedSmartLocks = [smartLocks[0]];
     return done();
   });
+});
 
-  afterAll(async done => {
-    console.log("group smartLocks", smartLocks);
+// Create groups
+describe("Create groups", function() {
+  it("should create group with 'active' state", function(done) {
+    const groupPayload = {
+      id: firstGroupFromAzureAd.id,
+      status: "Active"
+    };
+    addGroup(groupPayload).then(res => {
+      expect(res.status).toBe(201);
+      return done();
+    });
+  });
 
+  it("should create group with list of smart locks", function(done) {
+    const groupPayload = {
+      id: secondGroupFromAzureAd.id,
+      status: "Active",
+      smartLockGroups: extractIdList(smartLocks, "smartLockId")
+    };
+    addGroup(groupPayload).then(res => {
+      expect(res.status).toBe(201);
+      return done();
+    });
+  });
+
+  it("should not create group if group exist", function(done) {
+    const groupPayload = {
+      id: firstGroupFromAzureAd.id,
+      status: "Active"
+    };
+    addGroup(groupPayload).catch(error => {
+      expect(error.response.status).toBe(409);
+      return done();
+    });
+  });
+
+  it("should confirm groups created successfully", function(done) {
+    getGroups().then(res => {
+      expect(res.status).toBe(200);
+      expect(res.data.length).toBe(groupCount + 2);
+      return done();
+    });
+  });
+});
+
+// Update group
+describe("Update group", function() {
+  let groupToUpdate;
+  const statusUpdatePayload = [
+    {
+      value: "Inactive",
+      path: "/status",
+      op: "replace"
+    }
+  ];
+
+  it("should get group to update", function(done) {
+    getGroup(firstGroupFromAzureAd.id).then(res => {
+      groupToUpdate = res.data;
+      expect(res.status).toBe(200);
+      expect(res.data.id).toBe(firstGroupFromAzureAd.id);
+      return done();
+    });
+  });
+
+  it("should update group", function(done) {
+    updateGroup(groupToUpdate.id, statusUpdatePayload).then(res => {
+      expect(res.status).toBe(204);
+      return done();
+    });
+  });
+
+  it("should update smart lock list for group", function(done) {
+    const smartLocksUpdatePayload = [
+      {
+        value: extractIdList(updatedSmartLocks, "smartLockId"),
+        path: "/smartLockGroups",
+        op: "replace"
+      }
+    ];
+    updateGroup(groupToUpdate.id, smartLocksUpdatePayload).then(res => {
+      expect(res.status).toBe(204);
+      return done();
+    });
+  });
+  it("should confirm group status updated successfully ", function(done) {
+    getGroup(firstGroupFromAzureAd.id).then(res => {
+      expect(res.status).toBe(200);
+      expect(res.data.id).toBe(firstGroupFromAzureAd.id);
+      expect(res.data.status).toBe(statusUpdatePayload[0].value);
+      return done();
+    });
+  });
+
+  it("should confirm group smart locks updated successfully ", function(done) {
+    getGroupSmartLocks(firstGroupFromAzureAd.id).then(res => {
+      expect(res.status).toBe(200);
+      expect(res.data).toHaveLength(updatedSmartLocks.length);
+      return done();
+    });
+  });
+});
+
+// Delete group
+describe("Delete group", function() {
+  let groupToDelete;
+  it("Get group to delete", function(done) {
+    getGroup(firstGroupFromAzureAd.id).then(res => {
+      groupToDelete = res.data;
+      expect(res.status).toBe(200);
+      expect(res.data.id).toBe(firstGroupFromAzureAd.id);
+      return done();
+    });
+  });
+
+  it("should delete group", function(done) {
+    deleteGroup(groupToDelete.id).then(res => {
+      expect(res.status).toBe(204);
+      return done();
+    });
+  });
+  it("should confirm group deleted successfully", function(done) {
+    getGroup(groupToDelete.id).catch(error => {
+      expect(error.response.status).toBe(404);
+      return done();
+    });
+  });
+});
+
+describe("Setup", function() {
+  it("should delete added data from database", async function(done) {
     try {
       await deleteSmartLock(smartLocks[0].id);
     } catch (e) {
@@ -63,135 +194,8 @@ describe("Groups", function() {
     } catch (e) {
       console.log("group deleteGroup2", e);
     }
+    await console.log("group smartLocks after ", smartLocks);
+
     return done();
-  });
-
-  // Create groups
-  describe("Create groups", function() {
-    it("should create group with 'active' state", function(done) {
-      const groupPayload = {
-        id: firstGroupFromAzureAd.id,
-        status: "Active"
-      };
-      addGroup(groupPayload).then(res => {
-        expect(res.status).toBe(201);
-        return done();
-      });
-    });
-
-    it("should create group with list of smart locks", function(done) {
-      const groupPayload = {
-        id: secondGroupFromAzureAd.id,
-        status: "Active",
-        smartLockGroups: extractIdList(smartLocks, "smartLockId")
-      };
-      addGroup(groupPayload).then(res => {
-        expect(res.status).toBe(201);
-        return done();
-      });
-    });
-
-    it("should not create group if group exist", function(done) {
-      const groupPayload = {
-        id: firstGroupFromAzureAd.id,
-        status: "Active"
-      };
-      addGroup(groupPayload).catch(error => {
-        expect(error.response.status).toBe(409);
-        return done();
-      });
-    });
-
-    it("should confirm groups created successfully", function(done) {
-      getGroups().then(res => {
-        expect(res.status).toBe(200);
-        expect(res.data.length).toBe(groupCount + 2);
-        return done();
-      });
-    });
-  });
-
-  // Update group
-  describe("Update group", function() {
-    let groupToUpdate;
-    const statusUpdatePayload = [
-      {
-        value: "Inactive",
-        path: "/status",
-        op: "replace"
-      }
-    ];
-
-    it("should get group to update", function(done) {
-      getGroup(firstGroupFromAzureAd.id).then(res => {
-        groupToUpdate = res.data;
-        expect(res.status).toBe(200);
-        expect(res.data.id).toBe(firstGroupFromAzureAd.id);
-        return done();
-      });
-    });
-
-    it("should update group", function(done) {
-      updateGroup(groupToUpdate.id, statusUpdatePayload).then(res => {
-        expect(res.status).toBe(204);
-        return done();
-      });
-    });
-
-    it("should update smart lock list for group", function(done) {
-      const smartLocksUpdatePayload = [
-        {
-          value: extractIdList(updatedSmartLocks, "smartLockId"),
-          path: "/smartLockGroups",
-          op: "replace"
-        }
-      ];
-      updateGroup(groupToUpdate.id, smartLocksUpdatePayload).then(res => {
-        expect(res.status).toBe(204);
-        return done();
-      });
-    });
-    it("should confirm group status updated successfully ", function(done) {
-      getGroup(firstGroupFromAzureAd.id).then(res => {
-        expect(res.status).toBe(200);
-        expect(res.data.id).toBe(firstGroupFromAzureAd.id);
-        expect(res.data.status).toBe(statusUpdatePayload[0].value);
-        return done();
-      });
-    });
-
-    it("should confirm group smart locks updated successfully ", function(done) {
-      getGroupSmartLocks(firstGroupFromAzureAd.id).then(res => {
-        expect(res.status).toBe(200);
-        expect(res.data).toHaveLength(updatedSmartLocks.length);
-        return done();
-      });
-    });
-  });
-
-  // Delete group
-  describe("Delete group", function() {
-    let groupToDelete;
-    it("Get group to delete", function(done) {
-      getGroup(firstGroupFromAzureAd.id).then(res => {
-        groupToDelete = res.data;
-        expect(res.status).toBe(200);
-        expect(res.data.id).toBe(firstGroupFromAzureAd.id);
-        return done();
-      });
-    });
-
-    it("should delete group", function(done) {
-      deleteGroup(groupToDelete.id).then(res => {
-        expect(res.status).toBe(204);
-        return done();
-      });
-    });
-    it("should confirm group deleted successfully", function(done) {
-      getGroup(groupToDelete.id).catch(error => {
-        expect(error.response.status).toBe(404);
-        return done();
-      });
-    });
   });
 });
--- admin-ui/src/scenes/Users/users.a.test.js
+++ admin-ui/src/scenes/Users/users.a.test.js
@@ -5,7 +5,6 @@ import {
   getUsers,
   deleteUser,
   updateUser,
-  getSmartLocks,
   addSmartLock,
   deleteSmartLock,
   getUserSmartLocks
@@ -14,185 +13,189 @@ import helpers from "../../helpers";
 
 const { extractIdList } = helpers;
 
-describe("Users", function() {
-  let firstUserFromAzureAd;
-  let secondUserFromAzureAd;
-  let userCount;
-  let smartLocks;
-  let updatedSmartLocks;
-  const smartLocksToAdd = [
-    {
-      title: "Smart lock 1",
-      status: "Active"
-    },
-    {
-      title: "Smart lock 2",
-      status: "Inactive"
-    }
-  ];
-
-  beforeAll(async done => {
+let firstUserFromAzureAd;
+let secondUserFromAzureAd;
+let userCount;
+let smartLocks;
+let updatedSmartLocks;
+let firstSmartLock;
+let secondeSmartLock;
+const smartLocksToAdd = [
+  {
+    title: "Smart lock 1",
+    status: "Active"
+  },
+  {
+    title: "Smart lock 2",
+    status: "Inactive"
+  }
+];
+describe("Setup", function() {
+  it("should set tests data", async function(done) {
     const azureAdUsers = await getAzureAdUsers();
     [firstUserFromAzureAd, secondUserFromAzureAd] = await azureAdUsers.data;
 
     const users = await getUsers();
     userCount = await users.data.length;
 
-    await addSmartLock(smartLocksToAdd[0]);
-    await addSmartLock(smartLocksToAdd[1]);
-    const smartLocksResponse = await getSmartLocks();
-    smartLocks = smartLocksResponse.data;
+    const addFirstSmartLock = await addSmartLock(smartLocksToAdd[0]);
+    firstSmartLock = addFirstSmartLock.data;
+    const addSecondSmartLock = await addSmartLock(smartLocksToAdd[1]);
+    secondeSmartLock = addSecondSmartLock.data;
+    smartLocks = [firstSmartLock, secondeSmartLock];
     updatedSmartLocks = [smartLocks[0]];
     return done();
   });
+});
 
-  afterAll(async done => {
-    console.log("smartLocks after", smartLocks);
+// Create users
+describe("Create users", function() {
+  it("should create user with 'active' state", function(done) {
+    const userPayload = {
+      id: firstUserFromAzureAd.id,
+      status: "Active"
+    };
+    addUser(userPayload).then(res => {
+      expect(res.status).toBe(201);
+      return done();
+    });
+  });
 
+  it("should create user with list of smart locks", function(done) {
+    const userPayload = {
+      id: secondUserFromAzureAd.id,
+      status: "Active",
+      smartLockUsers: extractIdList(smartLocks, "smartLockId")
+    };
+    addUser(userPayload).then(res => {
+      expect(res.status).toBe(201);
+      return done();
+    });
+  });
+
+  it("should not create user if user exist", function(done) {
+    const userPayload = {
+      id: firstUserFromAzureAd.id,
+      status: "Active"
+    };
+    addUser(userPayload).catch(error => {
+      expect(error.response.status).toBe(409);
+      return done();
+    });
+  });
+
+  it("should confirm users created successfully", function(done) {
+    getUsers().then(res => {
+      expect(res.status).toBe(200);
+      expect(res.data.length).toBe(userCount + 2);
+      return done();
+    });
+  });
+});
+
+// Update user
+describe("Update user", function() {
+  let userToUpdate;
+  const statusUpdatePayload = [
+    {
+      value: "Inactive",
+      path: "/status",
+      op: "replace"
+    }
+  ];
+
+  it("should get user to update", function(done) {
+    getUser(firstUserFromAzureAd.id).then(res => {
+      userToUpdate = res.data;
+      expect(res.status).toBe(200);
+      expect(res.data.id).toBe(firstUserFromAzureAd.id);
+      return done();
+    });
+  });
+
+  it("should update user", function(done) {
+    updateUser(userToUpdate.id, statusUpdatePayload).then(res => {
+      expect(res.status).toBe(204);
+      return done();
+    });
+  });
+
+  it("should update smart lock list for user", function(done) {
+    const smartLocksUpdatePayload = [
+      {
+        value: extractIdList(updatedSmartLocks, "smartLockId"),
+        path: "/smartLockUsers",
+        op: "replace"
+      }
+    ];
+    updateUser(userToUpdate.id, smartLocksUpdatePayload).then(res => {
+      expect(res.status).toBe(204);
+      return done();
+    });
+  });
+  it("should confirm user status updated successfully ", function(done) {
+    getUser(firstUserFromAzureAd.id).then(res => {
+      expect(res.status).toBe(200);
+      expect(res.data.id).toBe(firstUserFromAzureAd.id);
+      expect(res.data.status).toBe(statusUpdatePayload[0].value);
+      return done();
+    });
+  });
+
+  it("should confirm user smart locks updated successfully ", function(done) {
+    getUserSmartLocks(firstUserFromAzureAd.id).then(res => {
+      expect(res.status).toBe(200);
+      expect(res.data).toHaveLength(updatedSmartLocks.length);
+      return done();
+    });
+  });
+});
+
+// Delete user
+describe("Delete user", function() {
+  let userToDelete;
+  it("Get user to delete", function(done) {
+    getUser(firstUserFromAzureAd.id).then(res => {
+      userToDelete = res.data;
+      expect(res.status).toBe(200);
+      expect(res.data.id).toBe(firstUserFromAzureAd.id);
+      return done();
+    });
+  });
+
+  it("should delete user", function(done) {
+    deleteUser(userToDelete.id).then(res => {
+      expect(res.status).toBe(204);
+      return done();
+    });
+  });
+  it("should confirm user deleted successfully", function(done) {
+    getUser(userToDelete.id).catch(error => {
+      expect(error.response.status).toBe(404);
+      return done();
+    });
+  });
+});
+
+describe("Tear down", function() {
+  it("should delete added data from database", async function(done) {
     try {
       await deleteSmartLock(smartLocks[0].id);
     } catch (e) {
-      console.log("deleteSmartLock1", e);
+      console.log("user deleteSmartLock1", e);
     }
     try {
       await deleteSmartLock(smartLocks[1].id);
     } catch (e) {
-      console.log("deleteSmartLock2", e);
+      console.log("user deleteSmartLock2", e);
     }
     try {
       await deleteUser(secondUserFromAzureAd.id);
     } catch (e) {
       console.log("deleteUser2", e);
     }
+    await console.log("users smartLocks after", smartLocks);
+
     return done();
-  });
-
-  // Create users
-  describe("Create users", function() {
-    it("should create user with 'active' state", function(done) {
-      const userPayload = {
-        id: firstUserFromAzureAd.id,
-        status: "Active"
-      };
-      addUser(userPayload).then(res => {
-        expect(res.status).toBe(201);
-        return done();
-      });
-    });
-
-    it("should create user with list of smart locks", function(done) {
-      const userPayload = {
-        id: secondUserFromAzureAd.id,
-        status: "Active",
-        smartLockUsers: extractIdList(smartLocks, "smartLockId")
-      };
-      addUser(userPayload).then(res => {
-        expect(res.status).toBe(201);
-        return done();
-      });
-    });
-
-    it("should not create user if user exist", function(done) {
-      const userPayload = {
-        id: firstUserFromAzureAd.id,
-        status: "Active"
-      };
-      addUser(userPayload).catch(error => {
-        expect(error.response.status).toBe(409);
-        return done();
-      });
-    });
-
-    it("should confirm users created successfully", function(done) {
-      getUsers().then(res => {
-        expect(res.status).toBe(200);
-        expect(res.data.length).toBe(userCount + 2);
-        return done();
-      });
-    });
-  });
-
-  // Update user
-  describe("Update user", function() {
-    let userToUpdate;
-    const statusUpdatePayload = [
-      {
-        value: "Inactive",
-        path: "/status",
-        op: "replace"
-      }
-    ];
-
-    it("should get user to update", function(done) {
-      getUser(firstUserFromAzureAd.id).then(res => {
-        userToUpdate = res.data;
-        expect(res.status).toBe(200);
-        expect(res.data.id).toBe(firstUserFromAzureAd.id);
-        return done();
-      });
-    });
-
-    it("should update user", function(done) {
-      updateUser(userToUpdate.id, statusUpdatePayload).then(res => {
-        expect(res.status).toBe(204);
-        return done();
-      });
-    });
-
-    it("should update smart lock list for user", function(done) {
-      const smartLocksUpdatePayload = [
-        {
-          value: extractIdList(updatedSmartLocks, "smartLockId"),
-          path: "/smartLockUsers",
-          op: "replace"
-        }
-      ];
-      updateUser(userToUpdate.id, smartLocksUpdatePayload).then(res => {
-        expect(res.status).toBe(204);
-        return done();
-      });
-    });
-    it("should confirm user status updated successfully ", function(done) {
-      getUser(firstUserFromAzureAd.id).then(res => {
-        expect(res.status).toBe(200);
-        expect(res.data.id).toBe(firstUserFromAzureAd.id);
-        expect(res.data.status).toBe(statusUpdatePayload[0].value);
-        return done();
-      });
-    });
-
-    it("should confirm user smart locks updated successfully ", function(done) {
-      getUserSmartLocks(firstUserFromAzureAd.id).then(res => {
-        expect(res.status).toBe(200);
-        expect(res.data).toHaveLength(updatedSmartLocks.length);
-        return done();
-      });
-    });
-  });
-
-  // Delete user
-  describe("Delete user", function() {
-    let userToDelete;
-    it("Get user to delete", function(done) {
-      getUser(firstUserFromAzureAd.id).then(res => {
-        userToDelete = res.data;
-        expect(res.status).toBe(200);
-        expect(res.data.id).toBe(firstUserFromAzureAd.id);
-        return done();
-      });
-    });
-
-    it("should delete user", function(done) {
-      deleteUser(userToDelete.id).then(res => {
-        expect(res.status).toBe(204);
-        return done();
-      });
-    });
-    it("should confirm user deleted successfully", function(done) {
-      getUser(userToDelete.id).catch(error => {
-        expect(error.response.status).toBe(404);
-        return done();
-      });
-    });
   });
 });
