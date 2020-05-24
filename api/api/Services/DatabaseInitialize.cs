b924f84ea0b7ed72dac51f6e2da6ddec660cb3bb
Mohamad Kenas <m.kenas91@gmail.com>
2020-05-25 01:05:35 +0200

    Comment out initializing av database

--- api/api/Services/DatabaseInitialize.cs
+++ api/api/Services/DatabaseInitialize.cs
@@ -10,13 +10,12 @@ namespace api.Services
         [Obsolete]
         public void Initialize(ApiContext context)
         {   
-            
+            // context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            /*
             // Initializing database with data for development purposes
             // Note that the ids for users and groups should belong to users and groups in your Azure Ad tenant
-            context.Database.EnsureDeleted();
-            context.Database.EnsureCreated();
-
-            
+    
             var user1 = new User
             {
                 Id = Guid.Parse("72ba6db2-945e-449a-b8f2-ea6fc1bde02d"),
@@ -162,6 +161,7 @@ namespace api.Services
             context.Accesses.AddRange(accessLog1, accessLog2);
 
             context.SaveChanges();
+            */
         }
     }
 }
